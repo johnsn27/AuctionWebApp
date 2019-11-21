e3fa5703ebdfba03707d4132b5e239bfc1cf0589
selvad02 <dilani.selvanathan@bbc.co.uk>
2019-11-21 14:42:06 +0000

    Add models

--- auctionsite/auctionsiteapp/models.py
+++ auctionsite/auctionsiteapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+# from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+# class UserManager(BaseUserManager):
+
+#     def create_user(self, username, email, password, dob):
+#         if not username:
+#             raise ValueError('The given username must be set')
+#         email = self.normalize_email(email)
+#         username = self.model.normalize_username(username)
+#         dob = self.
+#         user = self.model(username=username, email=email, dob=dob)
+#         user.set_password(password)
+
+class User(models.Model):
+    email = models.EmailField()
+    dateOfBirth = model.DateTimeField()
+
+class Item(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    picture = models.ImageField(upload_to='images/')
+    endDate = model.DateTimeField()
