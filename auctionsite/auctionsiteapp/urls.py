46a48e36b2cdab825ea0744a735f38f70f9b7367
Lana Allingham <lana.allingham@bbc.co.uk>
2019-11-27 22:55:19 +0000

    Start to tidy up code

--- auctionsite/auctionsiteapp/urls.py
+++ auctionsite/auctionsiteapp/urls.py
@@ -12,9 +12,7 @@ urlpatterns = [
     path('getitems', views.HomePageView.as_view(), name="getitems"),
     path('auction', views.AuctionView.as_view(), name="auction"),
     path('editBid', views.editBid, name="editBid"),
-
-
-    path('postitem/', views.CreatePostView.as_view(), name="postitem"),
+    path('postitem', views.CreatePostView.as_view(), name="postitem"),
     path('search', views.SearchView.as_view(), name="search"),
     path('expiredlistings', views.ExpiredView.as_view(), name="expired"),
     path('items', views.items_json, name='itemsjson'),
@@ -22,6 +20,6 @@ urlpatterns = [
     path('listings', views.viewListings, name="listings"),
     path('put1', views.put1.as_view(), name="put1"),
     path('put2/<int:pk>', views.put2.as_view(), name="put2"),
-    path('put3/<int:pk>', views.put3, name="put3"),
+    path('put3/<int:pk>', views.editBid, name="put3"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
