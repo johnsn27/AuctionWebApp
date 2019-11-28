6eb6395a80976fb01938efd531c13e22881e18b8
Lana Allingham <lana.allingham@bbc.co.uk>
2019-11-28 10:44:43 +0000

    Tidying (#12)
    * Start to tidy up code
    
    * Remove put2

--- auctionsite/auctionsiteapp/urls.py
+++ auctionsite/auctionsiteapp/urls.py
@@ -12,18 +12,13 @@ urlpatterns = [
     path('getitems', views.HomePageView.as_view(), name="getitems"),
     path('auction', views.AuctionView.as_view(), name="auction"),
     path('closedauction', views.ClosedAuctionView.as_view(), name="auctionauction"),
-    path('editBid', views.editBid, name="editBid"),
-
-
-
-    path('postitem/', views.CreatePostView.as_view(), name="postitem"),
+    path('postitem', views.CreatePostView.as_view(), name="postitem"),
     path('search', views.SearchView.as_view(), name="search"),
     path('expiredlistings', views.ExpiredView.as_view(), name="expired"),
     path('items', views.items_json, name='itemsjson'),
     path('profile', views.viewProfile, name='profile'),
-    path('listings', views.viewListings, name="listings"),
     path('put1', views.put1.as_view(), name="put1"),
     path('put2/<int:pk>', views.put2.as_view(), name="put2"),
-    path('put3/<int:pk>', views.put3, name="put3"),
+    path('editBid', views.editBid, name="editBid"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- auctionsite/auctionsiteapp/views.py
+++ auctionsite/auctionsiteapp/views.py
@@ -125,69 +125,29 @@ def createUser(request):
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
-
-def viewListings(request):
-    items = Item.objects.all()
-    context = {
-        'items': items,
-    }
-    return render(request, 'listings.html', {'items': items})
-
-
 def viewProfile(request):
     context = {
         'user': request.user
     }
     return render(request, 'profile.html', context)
 
-
-def editBid(request):
-    put = QueryDict(request.body)
-    item_id = put.get('item-id')
-    item = Item.objects.get(id=item_id)
-    newPrice = put.get('item-price')
-    error = None
-    if newPrice > item.price:
-        item.price = newPrice
-        item.save()
-    else:
-        error = 'Bid is lower than current price'
-
-    return JsonResponse({
-        'id': item.id,
-        'price': item.price,
-        'error': error
-    })
-
 class put1(ListView):
     model = Item
     template_name = 'put1.html'
 
-class put2(ListView):
-    model = Item
-    template_name = 'put2.html'
-
-def put3(request, pk):
+def editBid(request):
     if request.method == 'PUT':
-        obj = Item.objects.get(pk=pk)
+        pk = QueryDict(request.body).get('item')
+        item = Item.objects.get(pk=pk)
         newPrice = float(QueryDict(request.body).get('price'))
-        if newPrice > obj.price:
-            obj.price = newPrice
-            obj.save()
+        if newPrice > item.price:
+            item.price = newPrice
+            item.save()
         else:
             error = 'Bid is lower than current price'
-        obj.save()
         return JsonResponse({
-            'id': obj.id,
-            'price': obj.price,
+            'id': item.id,
+            'price': item.price,
             'error': error
         })
-    return HttpResponse("Not a put request")
-
-# def editBid(request, pk):
-#     if request.method != 'PUT':
-#         obj = Post.objects.get(pk=pk)
-#         obj.price = request.POST.get('item-price')
-#         obj.save()
-#         return HttpResponse("Post with pk %s updated" % pk, content_type='application/json')
-#     return HttpResponse("Not a put request")
+    return HttpResponse("Not a PUT request")
